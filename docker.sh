87e1f1d360cfbabd825f2ffa02bcb4394a270222
Hilmi Raditya Prakoso <raditya113@icloud.com>
2020-11-07 15:33:16 +0700

    port 80

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 docker pull hilmiraditya/json-server:latest
 docker container stop json-server
 docker container rm json-server
-docker run -d -p 3000:3000 --name=json-server hilmiraditya/json-server:latest
+docker run -d -p 80:3000 --name=json-server hilmiraditya/json-server:latest
